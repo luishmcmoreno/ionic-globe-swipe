160ee769b46cdcdf2d90ec0e67d735c7d2f6e0ed
Luis H. Moreno <luishmcmoreno@gmail.com>
2018-04-23 19:32:22 -0300

    globe rotating

---
+++ src/models/campus.ts
@@ -0,0 +1,42 @@
+export const campus = [
+    {
+        title: 'Campus Party Canada',
+        lat: 52.393336,
+        lng: -111.839054,
+    },
+    {
+        title: 'Campus Party USA',
+        lat: 37.393300,
+        lng: -121.979514,
+    },
+    {
+        title: 'Campus Party Mexico',
+        lat: 19.255141,
+        lng: -98.486093,
+    },
+    {
+        title: 'Campus Party Brasil',
+        lat: -23.565774,
+        lng: -46.876192,
+    },
+    {
+        title: 'Campus Party Espanha',
+        lat: 40.332506,
+        lng: -4.016935,
+    },
+    {
+        title: 'Campus Party Itália',
+        lat: 45.391941,
+        lng: 9.054293,
+    },
+    {
+        title: 'Campus Party China',
+        lat: 40.176681,
+        lng: 115.739540,
+    },
+    {
+        title: 'Campus Party Japão',
+        lat: 35.723506,
+        lng: 139.694535,
+    }
+];
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { campus } from './../../models/campus';
+import { Component, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -7,61 +8,156 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  static readonly rotateTime = 200;
+  static readonly rotateUpdateTime = 10;
+
   globe: any;
   canvas: HTMLCanvasElement;
   currentInterval: number;
-  currentCP = 'Campus Party Africa';
+  index = 0;
+  campus = campus;
+  currentLat;
+  currentLng;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public zone: NgZone,
+  ) {}
 
+  rotateLat(country) {
+    const lat1 = this.currentLng;
+    const lat2 = country.lng;
+    if (lat1 > lat2) {
+      if (Math.abs(lat1 - lat2) >= 90 && Math.abs(lat1 - lat2) <= 180) {
+        this.rotateLatToTop(country);
+      } else {
+        this.rotateLatToBottom(country);
+      }
+    } else {
+      if (Math.abs(-lat1 + lat2) >= 90 && Math.abs(-lat1 + lat2) <= 180) {
+        this.rotateLatToBottom(country);
+      } else {
+        this.rotateLatToTop(country);
+      }
+    }
+  }
+
+  rotateLng(country) {
+    const lng1 = this.currentLng;
+    const lng2 = country.lng;
+    if (lng1 > lng2) {
+      if (Math.abs(lng1 - lng2) >= 180 && Math.abs(lng1 - lng2) <= 360) {
+        this.rotateLngToRight(country);
+      } else {
+        this.rotateLngToLeft(country);
+      }
+    } else {
+      if (Math.abs(-lng1 + lng2) >= 180 && Math.abs(-lng1 + lng2) <= 360) {
+        this.rotateLngToLeft(country);
+      } else {
+        this.rotateLngToRight(country);
+      }
+    }
+  }
+
+  rotateLngToLeft(country) {
+    console.log('rotating to left');
+    const diference = Math.abs(this.currentLng - country.lng);
+    const step = diference/(HomePage.rotateTime/HomePage.rotateUpdateTime);
+    const interval = setInterval(() => {
+      this.zone.run(() => {
+        if (this.currentLng - step > country.lng) {
+          this.currentLng -= step;
+        } else {
+          this.currentLng = country.lng;
+          clearInterval(interval);
+        }
+        this.globe.projection.rotate([-this.currentLng, -this.currentLat]);
+      });
+    }, HomePage.rotateUpdateTime);
+  }
+
+  rotateLngToRight(country) {
+    console.log('rotating to right');
+    const diference = Math.abs(this.currentLng - country.lng);
+    const step = diference/(HomePage.rotateTime/HomePage.rotateUpdateTime);
+    const interval = setInterval(() => {
+      this.zone.run(() => {
+        if (this.currentLng + step < country.lng) {
+          this.currentLng += step;
+        } else {
+          this.currentLng = country.lng;
+          clearInterval(interval);
+        }
+        this.globe.projection.rotate([-this.currentLng, -this.currentLat]);
+      });
+    }, HomePage.rotateUpdateTime);
+  }
+
+  rotateLatToTop(country) {
+    console.log('Rotating to Top');
+    const diference = Math.abs(this.currentLat - country.lat);
+    const step = diference/(HomePage.rotateTime/HomePage.rotateUpdateTime);
+    const interval = setInterval(() => {
+      this.zone.run(() => {
+        if (this.currentLat + step < country.lat) {
+          this.currentLat += step;
+        } else {
+          this.currentLat = country.lat;
+          clearInterval(interval);
+        }
+        this.globe.projection.rotate([-this.currentLng, -this.currentLat]);
+      });
+    }, HomePage.rotateUpdateTime);
+  } 
+
+  rotateLatToBottom(country) {
+    console.log('rotating to bottom');
+    const diference = Math.abs(this.currentLat - country.lat);
+    const step = diference/(HomePage.rotateTime/HomePage.rotateUpdateTime);
+    const interval = setInterval(() => {
+      this.zone.run(() => {
+        if (this.currentLat - step > country.lat) {
+          this.currentLat -= step;
+        } else {
+          this.currentLat = country.lat;
+          clearInterval(interval);
+        } 
+        this.globe.projection.rotate([-this.currentLng, -this.currentLat]);
+      });
+    }, HomePage.rotateUpdateTime);
   }
 
   onSwipe(ev) {
-    if (ev.offsetDirection === 2 || ev.offsetDirection === 4) {
-      if (this.currentInterval) {
-        clearInterval(this.currentInterval);
-      }
-    }
     if (ev.offsetDirection === 2) {
-      this.changeToRight();
+      this.index = (this.index + 1) % campus.length;
     } else if (ev.offsetDirection === 4) {
-      this.changeToLeft();
+      this.index = (this.index - 1) % campus.length;
     }
+    if (this.index < 0) {
+      this.index = campus.length - 1;
+    }
+    this.loadCountry(this.campus[this.index]);
   }
 
-  changeToLatLng(lng, lat) {
-    console.log('changing to right');
-    this.globe.projection.rotate([lng * - 1, lat * -1]);
+  loadCountry(country) {
+    const lat = country.lat;
+    const lng = country.lng;
+    clearInterval(this.currentInterval);
+    if (this.currentLat && this.currentLng) {
+      this.rotateLat(country);
+      this.rotateLng(country);
+    }
+    this.globe.plugins.pings.add(lng, lat, { color: '#FFFF33', ttl: 2000, angle: Math.random() * 10 });
     this.currentInterval = setInterval(() => {
-      console.log('interval to right');
-      this.globe.plugins.pings.add(lng, lat, { color: '#FFFF33', ttl: 2000, angle: Math.random() * 10 });
-    }, 500);
-  }
-
-  changeToRight() {
-    this.currentCP = 'Campus Party Japão';
-    var lat = 39.3024886;
-    var lng = 137.8805624;
-    this.changeToLatLng(lng, lat);
-  }
-
-  changeToLeft() {
-    var lat = -20.1793608;
-    var lng = -46.9537133;
-    console.log(this.globe.projection);
-    this.globe.projection.rotate([lng * - 1, lat * -1]);
-    this.currentCP = 'Campus Party Brasil';
-    this.currentInterval = setInterval(() => {
-      console.log('interval to left');
       this.globe.plugins.pings.add(lng, lat, { color: '#FFFF33', ttl: 2000, angle: Math.random() * 10 });
     }, 500);
   }
 
   initGlobe() {
     this.globe = planetaryjs.planet();
-    console.log(this.globe);
     this.globe.loadPlugin(planetaryjs.plugins.earth({
-      topojson: { file:   '/assets/js/world-110m.json' },
+      topojson: { file:   'assets/js/world-110m.json' },
       oceans:   { fill:   '#000080' },
       land:     { fill:   '#339966' },
       borders:  { stroke: '#008000' }
@@ -71,21 +167,21 @@ export class HomePage {
       scaleExtent: [100, 300]
     }));
 
-    var lat = 13.0180523;
-    var lng = 6.6422333;
+    const country = this.campus[this.index];
+    this.currentLat = country.lat;
+    this.currentLng = country.lng;
 
     this.currentInterval = setInterval(() => {
-      this.globe.plugins.pings.add(lng, lat, { color: '#FFFF33', ttl: 2000, angle: Math.random() * 10 });
+      this.globe.plugins.pings.add(country.lng, country.lat, { color: '#FFFF33', ttl: 2000, angle: Math.random() * 10 });
     }, 500);
 
-    this.globe.projection.scale(175).translate([200, 200]).rotate([lat, lng, 0]);
+    this.globe.projection.scale(175).translate([200, 200]).rotate([country.lng * -1, country.lat * -1]);
     this.canvas = document.getElementById('rotatingGlobe') as HTMLCanvasElement;
     this.globe.draw(this.canvas);
     
   }
 
   ionViewDidLoad() {
-    console.log('oi');
     this.initGlobe();
   }
 
